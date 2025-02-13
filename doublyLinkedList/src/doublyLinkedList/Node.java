d6e349a980e07e9e7c0f08f4ab78f2eedc43a2d2
midori <mahuba@naver.com>
2025-02-13 20:23:46 +0900

    second commit

--- doublyLinkedList/src/doublyLinkedList/Node.java
+++ doublyLinkedList/src/doublyLinkedList/Node.java
@@ -4,10 +4,13 @@ public class Node <E>{
 	private E item;
 	public Node<E> next;
 	public Node<E> prev;
+	public int number;
+	
 
-	Node(Node<E>prev , E item , Node<E> next ){
+	Node(Node<E>prev , E item , Node<E> next , int number ){
 		this.item = item;
 		this.next = next;
 		this.prev = prev;
+		this.number = number;
 	}
 }
